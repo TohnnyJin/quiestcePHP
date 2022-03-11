c7e5ae071a99003e5203e46554bb8a2b2b22164d
Tohnny <98454836+TohnnyJin@users.noreply.github.com>
2022-03-11 09:10:06 +0100

    Add files via upload

---
+++ quiestce.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+
+<html>
+<head>
+
+
+<style>
+h1 {text-align: center;}
+</style>
+  <meta charset="utf-8">
+  <title>Qui-est ce ?</title>
+  <link rel="stylesheet" href="quiestce.php">
+</head>
+
+<body>
+<h1>Qui-est ce ?</h1>
+
+<img src="imagePHP/0.jpg" alt="image0">
+<img src="imagePHP/1.jpg" alt="image1">
+<img src="imagePHP/2.jpg" alt="image2">
+<img src="imagePHP/3.jpg" alt="image3">
+<img src="imagePHP/4.jpg" alt="image4">
+<img src="imagePHP/5.jpg" alt="image5">
+<img src="imagePHP/6.jpg" alt="image6">
+<img src="imagePHP/7.jpg" alt="image7">
+<img src="imagePHP/8.jpg" alt="image8">
+<img src="imagePHP/9.jpg" alt="image9">
+<img src="imagePHP/10.jpg" alt="image10">
+<img src="imagePHP/11.jpg" alt="image11">
+<img src="imagePHP/12.jpg" alt="image12">
+<img src="imagePHP/13.jpg" alt="image13">
+<img src="imagePHP/14.jpg" alt="image14">
+<img src="imagePHP/15.jpg" alt="image15">
+
+</body>
+</html>
