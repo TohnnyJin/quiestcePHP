07ae49d00d68df51d2d5b81a3ee0077d5c6442e0
Tohnny <98454836+TohnnyJin@users.noreply.github.com>
2022-03-11 10:16:32 +0100

    Add files via upload

--- quiestce.php
+++ quiestce.php
@@ -17,6 +17,27 @@ h1 {text-align: center;}
 <body>
 <h1>Qui-est ce ?</h1>
 
+<!--Image-->
+<img src="imagePHP/0.jpg" alt="image0">
+<img src="imagePHP/1.jpg" alt="image1">
+<img src="imagePHP/2.jpg" alt="image2">
+<img src="imagePHP/3.jpg" alt="image3">
+<img src="imagePHP/4.jpg" alt="image4">
+<img src="imagePHP/5.jpg" alt="image5">
+</br>
+<img src="imagePHP/6.jpg" alt="image6">
+<img src="imagePHP/7.jpg" alt="image7">
+<img src="imagePHP/8.jpg" alt="image8">
+<img src="imagePHP/9.jpg" alt="image9">
+<img src="imagePHP/10.jpg" alt="image10">
+</br>
+<img src="imagePHP/11.jpg" alt="image11">
+<img src="imagePHP/12.jpg" alt="image12">
+<img src="imagePHP/13.jpg" alt="image13">
+<img src="imagePHP/14.jpg" alt="image14">
+<img src="imagePHP/15.jpg" alt="image15">
+</br>
+
 <!--Questions-->
 <form method="post" action="quiestce.php">
 
@@ -32,28 +53,6 @@ h1 {text-align: center;}
 <input type="radio" name="question2" value="Q2Non2" id="Q2Non2" /> <label for="Q2Non1">Non</label><br />
 
 </fieldset>
-
-<!--Image-->
-<img src="imagePHP/0.jpg" alt="image0">
-<img src="imagePHP/1.jpg" alt="image1">
-<img src="imagePHP/2.jpg" alt="image2">
-<img src="imagePHP/3.jpg" alt="image3">
-</br>
-<img src="imagePHP/4.jpg" alt="image4">
-<img src="imagePHP/5.jpg" alt="image5">
-<img src="imagePHP/6.jpg" alt="image6">
-<img src="imagePHP/7.jpg" alt="image7">
-</br>
-<img src="imagePHP/8.jpg" alt="image8">
-<img src="imagePHP/9.jpg" alt="image9">
-<img src="imagePHP/10.jpg" alt="image10">
-<img src="imagePHP/11.jpg" alt="image11">
-</br>
-<img src="imagePHP/12.jpg" alt="image12">
-<img src="imagePHP/13.jpg" alt="image13">
-<img src="imagePHP/14.jpg" alt="image14">
-<img src="imagePHP/15.jpg" alt="image15">
-</br>
 
 
 </body>
