c1f1d1b71a149752a991ecc638d1b2df0a22a48a
Tohnny <98454836+TohnnyJin@users.noreply.github.com>
2022-03-11 09:57:20 +0100

    Add files via upload

--- quiestce.php
+++ quiestce.php
@@ -1,37 +1,60 @@
 <!DOCTYPE html>
 
-
 <html>
 <head>
 
+<link rel="stylesheet" href="quiestce.css">
 
 <style>
 h1 {text-align: center;}
 </style>
+
   <meta charset="utf-8">
   <title>Qui-est ce ?</title>
-  <link rel="stylesheet" href="quiestce.php">
+  
 </head>
 
 <body>
 <h1>Qui-est ce ?</h1>
 
+<!--Questions-->
+<form method="post" action="quiestce.php">
+
+<fieldset>
+    
+<h2 id="Question1">Question 1: Votre personnage porte-il des lunettes ?</h2>
+<input type="radio" name="question1" value="Q1Oui1" id="Q1Oui1" /> <label for="Q1Oui1">Oui</label><br />
+<input type="radio" name="question1" value="Q1Non1" id="Q1Non1" /> <label for="Q1Non1">Non</label><br />
+
+</br>
+<h2 id="Question1">Question 2: Votre personnage a-ti une moustache ?</h2>
+<input type="radio" name="question2" value="Q2Oui2" id="Q2Oui2" /> <label for="Q2Oui1">Oui</label><br />
+<input type="radio" name="question2" value="Q2Non2" id="Q2Non2" /> <label for="Q2Non1">Non</label><br />
+
+</fieldset>
+
+<!--Image-->
 <img src="imagePHP/0.jpg" alt="image0">
 <img src="imagePHP/1.jpg" alt="image1">
 <img src="imagePHP/2.jpg" alt="image2">
 <img src="imagePHP/3.jpg" alt="image3">
+</br>
 <img src="imagePHP/4.jpg" alt="image4">
 <img src="imagePHP/5.jpg" alt="image5">
 <img src="imagePHP/6.jpg" alt="image6">
 <img src="imagePHP/7.jpg" alt="image7">
+</br>
 <img src="imagePHP/8.jpg" alt="image8">
 <img src="imagePHP/9.jpg" alt="image9">
 <img src="imagePHP/10.jpg" alt="image10">
 <img src="imagePHP/11.jpg" alt="image11">
+</br>
 <img src="imagePHP/12.jpg" alt="image12">
 <img src="imagePHP/13.jpg" alt="image13">
 <img src="imagePHP/14.jpg" alt="image14">
 <img src="imagePHP/15.jpg" alt="image15">
+</br>
+
 
 </body>
 </html>
