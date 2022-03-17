91ba8293d8880a7f4e092defd31b44e701d011d7
Tohnny <98454836+TohnnyJin@users.noreply.github.com>
2022-03-17 10:18:28 +0100

    Add files via upload

--- quiestce.php
+++ quiestce.php
@@ -3,7 +3,7 @@
 <html>
 <head>
 
-<link rel="stylesheet" href="quiestce.css">
+<link rel="stylesheet" href="http://btsio.org/2022/jint/quiestce/quiestce.css">
 
 <style>
 h1 {text-align: center;}
@@ -15,27 +15,28 @@ h1 {text-align: center;}
 </head>
 
 <body>
+
 <h1>Qui-est ce ?</h1>
 
 <!--Image-->
-<img src="imagePHP/0.jpg" alt="image0">
-<img src="imagePHP/1.jpg" alt="image1">
-<img src="imagePHP/2.jpg" alt="image2">
-<img src="imagePHP/3.jpg" alt="image3">
-<img src="imagePHP/4.jpg" alt="image4">
-<img src="imagePHP/5.jpg" alt="image5">
+<img src="http://btsio.org/2022/jint/quiestce/images/0.jpg" alt="image0">
+<img src="http://btsio.org/2022/jint/quiestce/images/1.jpg" alt="image1">
+<img src="http://btsio.org/2022/jint/quiestce/images/2.jpg" alt="image2">
+<img src="http://btsio.org/2022/jint/quiestce/images/3.jpg" alt="image3">
+<img src="http://btsio.org/2022/jint/quiestce/images/4.jpg" alt="image4">
+<img src="http://btsio.org/2022/jint/quiestce/images/5.jpg" alt="image5">
 </br>
-<img src="imagePHP/6.jpg" alt="image6">
-<img src="imagePHP/7.jpg" alt="image7">
-<img src="imagePHP/8.jpg" alt="image8">
-<img src="imagePHP/9.jpg" alt="image9">
-<img src="imagePHP/10.jpg" alt="image10">
+<img src="http://btsio.org/2022/jint/quiestce/images/6.jpg" alt="image6">
+<img src="http://btsio.org/2022/jint/quiestce/images/7.jpg" alt="image7">
+<img src="http://btsio.org/2022/jint/quiestce/images/8.jpg" alt="image8">
+<img src="http://btsio.org/2022/jint/quiestce/images/9.jpg" alt="image9">
+<img src="http://btsio.org/2022/jint/quiestce/images/10.jpg" alt="image10">
 </br>
-<img src="imagePHP/11.jpg" alt="image11">
-<img src="imagePHP/12.jpg" alt="image12">
-<img src="imagePHP/13.jpg" alt="image13">
-<img src="imagePHP/14.jpg" alt="image14">
-<img src="imagePHP/15.jpg" alt="image15">
+<img src="http://btsio.org/2022/jint/quiestce/images/11.jpg" alt="image11">
+<img src="http://btsio.org/2022/jint/quiestce/images/12.jpg" alt="image12">
+<img src="http://btsio.org/2022/jint/quiestce/images/13.jpg" alt="image13">
+<img src="http://btsio.org/2022/jint/quiestce/images/14.jpg" alt="image14">
+<img src="http://btsio.org/2022/jint/quiestce/images/15.jpg" alt="image15">
 </br>
 
 <!--Questions/Formulaire , chaque question renvoie un 1 ou un 0-->
@@ -77,16 +78,24 @@ Non<input type="radio" name="question6" value="0" />
 Oui<input type="radio" name="question7" value="1" /> |
 Non<input type="radio" name="question7" value="0" /> 
 
-<br/>
-<input type="submit" value="Envoyer">
+<br/><br/>
+<input type="submit" value="Envoyer votre réponse">
 <br/> <br/>
 
 
 <?php
+try {
 echo "Vous dite être : <b>" . $_POST['question1']. $_POST['question2'] . $_POST['question3']. $_POST['question4'] . $_POST['question5'] . $_POST['question6']. $_POST['question7']. "</b><br>";
+} catch (Exception $e) {
+  echo 'Exception reçue : ',  $e->getMessage(), "\n";
+}
 ?>
-
 </fieldset>
+
+
+
+
+
 
 </body>
 </html>
